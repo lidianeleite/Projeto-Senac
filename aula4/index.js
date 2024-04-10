94210da6e5f1ce301b3812b01e8fea91e688d061
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-10 12:05:22 -0300

    conversao de variaveis

--- aula4/index.js
+++ aula4/index.js
@@ -1,1 +1,9 @@
 console.log ("hello world")
+const nome = "Lidiane"
+const idade = 39
+console.log("Meu nome é ",nome," e tenho", idade ," anos de ida")
+//Teste
+const idadeNumero=23
+const idadeTexto = idadeNumero.toString()
+console.log(typeof idadeNumero)
+console.log(typeof idadeTexto)
