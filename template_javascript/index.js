bb171debb83a5d289c2b9242e037a0c7e2b0a65c
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-15 10:00:02 -0300

    Testando commit

---
+++ template_javascript/index.js
@@ -0,0 +1,21 @@
+const primeiroNome = "Lidiane"
+const segundoNome = "Leite"
+const idadeNumero = "39"
+const serEstudante = "sim"
+console.log("o seu nome e ",primeiroNome," o seu sobrenome e ",segundoNome, "idade ",idadeNumero," é estudante?", serEstudante)
+/*let a = 10
+let b = 10
+console.log(b)
+b = 5
+console.log(a,b)
+let nome = prompt("Qual é seu nome?")
+alert(`Seu nome e ${nome}`)*/
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+console.log(a,b,c)
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`) //lalala
