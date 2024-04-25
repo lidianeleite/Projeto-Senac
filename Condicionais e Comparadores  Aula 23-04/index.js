0c08b9aa09b717c7e25a5af0f20ed856a161a699
lidianeleite <lidiane.leite1984@gmail.com>
2024-04-25 11:57:17 -0300

    Aula 25/04

--- Condicionais e Comparadores  Aula 23-04/index.js
+++ Condicionais e Comparadores  Aula 23-04/index.js
@@ -75,7 +75,7 @@ if (numero % 2 === 0) {
 
 // Exercio 4 - Interpretação de código 
 
-let generoFilme
+/*let generoFilme
 let precoFilme
 
 generoFilme = prompt("Digite o genero do Filme")
@@ -85,6 +85,84 @@ if ((generoFilme == "FANTASIA") && (precoFilme < 15)){
     alert(`Bom filme`)
 } else {
     alert(`Escolha outro filme`)
-}
+}*/
    
+//AULA 9 Exercicio 1 - Laços while
+
+/*let i = 0
+let soma = 0
+i = Number(prompt("Digite um numero para ser somado: (para sair digite 0!!!"))
+while (i != 0) {
+    soma = soma + i
+    i = Number(prompt("Digite um numero para ser somado: (Para sair digite 0!!!)"))
+}
+alert(soma)*/
+
+//Exercicio 2 Laços For
+
+/*let array = [11, 15, 18, 14, 12, 13]
+let maiorValor = 0 
+
+for (let i = 0; i < 6; i++){
+    if (array[i] > maiorValor){
+        maiorValor = array[i]
+    }
+}
+alert(maiorValor)*/
+
+
+//Exercicio 3 For...Of 
+
+/*const palavras = ["Oi", "sumido(a)", "tudo", "bem?", "Saudades"]
+let frase = ""
+
+for (let indices of palavras){
+    frase = frase + indices + " "
+}
+alert(frase)*/
+
+// LAÇOS AULA 9 25-04 
+// Exercício 1 Interpretação de Código
+
+/*let valor = 0
+for(let i = 0; i < 5; i++) {
+  valor += i
+}
+console.log(valor)
+alert(valor) = //10*/
+
+// Exercicio 2 Leia o código 
+
+// Resultado:A) 19-21-23-25-27-30
+
+/*const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+for (let numero of lista) {
+  if (numero > 18) {
+		console.log(numero)
+	}
+} */
+
+// Resultado B) 
+
+/*const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+for (let numero of lista) {
+  if (numero > 18) {
+		console.log(numero)
+	}
+}
+alert(valor)*/
+
+// Exercicio 3 
+
+/*const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+let quantidadeAtual = 0
+while(quantidadeAtual < quantidadeTotal){
+  let linha = ""
+  for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+    linha += "*"
+  }
+  console.log(linha)
+  quantidadeAtual++
+}*/
+
 
